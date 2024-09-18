24982a41447e203fc2b022d0bdc69da2e9c5a37f
pKakathkar <pkpran10@gmail.com>
2024-09-18 21:29:15 +0530

    chore: Add ContactUs page and dark mode toggle

--- task-manager-app/src/App.js
+++ task-manager-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { AboutUs } from "./pages/AboutUs";
 import { LoginPage } from "./pages/Login";
+import { ContactUs } from "./pages/ContactUs";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home/>} ></Route>
         <Route path="/about" element={<AboutUs/>} ></Route>
         <Route path="/login" element={<LoginPage/>} ></Route>
+        <Route path="/contact" element={<ContactUs/>}></Route>
       </Routes>
     </Router>
   );
